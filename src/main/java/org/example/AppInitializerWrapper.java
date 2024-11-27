e65bc66cf5241ea296aaee646038037245a92c30
rashmikaz <phrashmika@gmail.com>
2024-11-28 00:22:26 +0530

    check

--- src/main/java/org/example/AppInitializerWrapper.java
+++ src/main/java/org/example/AppInitializerWrapper.java
@@ -5,5 +5,6 @@ public class AppInitializerWrapper {
     public static void main(String[] args) {
         AppInitializer.main(args);
         System.out.println("hello");
+        System.out.println("hellooooo");
     }
 }
